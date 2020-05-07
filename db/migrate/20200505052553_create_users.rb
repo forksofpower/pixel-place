dce2f16ed003413fb93cc17d829d32188a09e626
Patrick Jones <patrickjones.pmj@gmail.com>
2020-05-07 16:17:11 -0500

    testing

--- db/migrate/20200505052553_create_users.rb
+++ db/migrate/20200505052553_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name
       t.string :last_name
       t.string :username
-      t.string :email
 
       t.timestamps
     end
